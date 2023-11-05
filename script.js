b22094185f93f0563793bf878db309155c02fa98
Emanuel <emanuelcascais@gmail.com>
2023-11-05 19:22:27 -0300

    Added button for creating new cards with album information via a modal form

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const addBtn = document.querySelector('.add-btn');
 const mainContainer = document.querySelector('.main-container');
+const modalMenu = document.querySelector('.modal-menu');
+const modalSubmit = document.querySelector('.modal-submit');
 
 function createAlbumCard(title, artistName, releaseDate, coverImageURL) {
 
@@ -29,19 +31,54 @@ function createAlbumCard(title, artistName, releaseDate, coverImageURL) {
 
 }
 
+function modalMenuReset() {
+    let userReleaseYearInput = document.querySelector('#modal-release-year');
+    let errorMessage = document.querySelector('.error-message');
+
+    document.querySelector('.modal-form').reset();
+    modalMenu.close();
+    errorMessage.textContent = '';
+    userReleaseYearInput.style.setProperty('border', '1px solid grey');
+
+}
+
 addBtn.addEventListener('click', () => {
-    
+    modalMenu.showModal();
+})
+
+modalSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    let userAlbumTitleInput = document.querySelector('#modal-album-name');
+    let userArtistNameInput = document.querySelector('#modal-artist-name');
+    let userReleaseYearInput = document.querySelector('#modal-release-year');
+    let userAlbumCoverURL = document.querySelector('#modal-album-cover');
+    let currentYear = new Date().getFullYear();
+    let errorMessage = document.querySelector('.error-message');
+ 
     let userAlbumData = {
-        albumTitle: "Innerspeaker",
-        artistName: "Tame Impala", 
-        releaseYear: "2010",
-        albumCoverURL: "https://m.media-amazon.com/images/I/A1gZc70vUIL._AC_UF1000,1000_QL80_.jpg"
+        albumTitle: userAlbumTitleInput.value,
+        artistName: userArtistNameInput.value, 
+        releaseYear: userReleaseYearInput.value,
+        albumCoverURL: userAlbumCoverURL.value
     };
 
-    let albumCard = createAlbumCard(userAlbumData.albumTitle, userAlbumData.artistName, userAlbumData.releaseYear, userAlbumData.albumCoverURL);
-
-    mainContainer.appendChild(albumCard);
     
+
+    if(userReleaseYearInput.value > currentYear || userReleaseYearInput.value < 1) {
+        userReleaseYearInput.style.setProperty('border', '1px solid red');
+        
+        errorMessage.innerText = (userReleaseYearInput.value > 0) ? "*Release year can't be greater than current year":"*Can't use negative numbers here..";
+        document.querySelector('.year-label').appendChild(errorMessage);
+        return;
+    }
+
+    let albumCard = createAlbumCard(userAlbumData.albumTitle, userAlbumData.artistName, userAlbumData.releaseYear, userAlbumData.albumCoverURL);
+    mainContainer.appendChild(albumCard);
+
+    modalMenuReset();
+
+   
+
 })
 
 
